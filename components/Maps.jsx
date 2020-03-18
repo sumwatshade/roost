200a29f9bf0a0bc4e1f69d81e7a9b5fbb342bb4a
Luke Shadler <lshadler13@gmail.com>
2020-03-17 23:11:55 -0700

    update for new styling

--- components/Maps.jsx
+++ components/Maps.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 import {API_KEY} from '../api-key';
@@ -5,42 +7,42 @@ import {API_KEY} from '../api-key';
 // TODO: replace with API call?
 import places from '../test-data/places';
 
-const Maps = ({ zoom }) => {
-    let sumLat = 0, sumLng = 0;
+const Maps = ({zoom}) => {
+  let sumLat = 0; let sumLng = 0;
 
-    const Markers = places.map(props => {
-        sumLat += parseFloat(props.lat);
-        sumLng += parseFloat(props.lng);
+  const Markers = places.map(({props}) => {
+    sumLat += parseFloat(props.lat);
+    sumLng += parseFloat(props.lng);
 
-        return <Marker {...props} />
-    });
+    return <Marker key={props.lat + 1000 * props.lng} {...props} />;
+  });
 
-    const center = {
-        lat: sumLat / places.length,
-        lng: sumLng / places.length
-    }
+  const center = {
+    lat: sumLat / places.length,
+    lng: sumLng / places.length,
+  };
 
-    return (
-        <div className="map-container">
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: API_KEY }}
-                defaultCenter={center}
-                defaultZoom={zoom}>
-                    {Markers}
-            </GoogleMapReact>
-            <style jsx>{`
+  return (
+    <div className="map-container">
+      <GoogleMapReact
+        bootstrapURLKeys={{key: API_KEY}}
+        defaultCenter={center}
+        defaultZoom={zoom}>
+        {Markers}
+      </GoogleMapReact>
+      <style jsx>{`
                 .map-container {
                     height: 500px;
                     width: 90vw;
                 }
             `}</style>
-        </div>
+    </div>
 
-    )
-}
+  );
+};
 
 Maps.defaultProps = {
-    zoom: 14
-}
+  zoom: 14,
+};
 
 export default Maps;
--- components/Marker.jsx
+++ components/Marker.jsx
@@ -1,37 +1,36 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 import {motion} from 'framer-motion';
 
 const Marker = ({name, venmo, color, lat, lng}) => {
+  const [clicked, setClicked] = useState(false);
 
-    const [clicked, setClicked] = useState(false)
-
-    return (
-        <motion.div 
-            layoutTransition
-            style={{
-                minWidth: '100px',
-                minHeight: '10px',
-                border: '1px solid grey',
-                borderRadius: '3px',
-                backgroundColor: color,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-            lat={lat}
-            lng={lng}
-            onClick={() => setClicked(!clicked)}>
-            <div className="name">{name}</div>
-            {clicked ? <div className="modal">{venmo}</div> : '' }
-        </motion.div>
-    )
-}
+  return (
+    <motion.div
+      layoutTransition
+      style={{
+        minWidth: '100px',
+        minHeight: '10px',
+        border: '1px solid grey',
+        borderRadius: '3px',
+        backgroundColor: color,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      lat={lat}
+      lng={lng}
+      onClick={() => setClicked(!clicked)}>
+      <div className="name">{name}</div>
+      {clicked ? <div className="modal">{venmo}</div> : '' }
+    </motion.div>
+  );
+};
 
 Marker.defaultProps = {
-    name: 'Luke',
-    color: 'red',
-    venmo: '@luke-shadler'
-}
+  name: 'Luke',
+  color: 'red',
+  venmo: '@luke-shadler',
+};
 
-export default Marker
+export default Marker;
