da06663ec32bbbcd8fb9b420a66207df9901da3a
Luke Shadler <lshadler13@gmail.com>
2020-04-04 16:09:04 -0700

    adjust coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,23 @@ module.exports = {
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
-    '__snapshots__',
   ],
   collectCoverage: true,
   collectCoverageFrom: [
     '**/components/**',
     '!**/node_modules/**',
     '!**/vendor/**',
+    '!**/__snapshots__/**',
+    // Ignore table prior to rewrite
+    '!**/Table.jsx',
+    '!**/SearchableTable.jsx',
   ],
+  coverageThreshold: {
+    'global': {
+      'branches': 90,
+      'functions': 90,
+      'lines': 90,
+      'statements': -10,
+    },
+  },
 };
